f77ac158cce87d602b17e0242c379e39b5f009de
Henrique Ferreira Silva <henrique_silva_ferreira@hotmail.com>
2025-02-15 14:55:44 -0300

    Preparando a interação entre camadas

---
+++ src/main/java/br/com/alura/codechella/application/gateways/RepositorioDeUsuario.java
@@ -0,0 +1,13 @@
+package br.com.alura.codechella.application.gateways;
+
+import br.com.alura.codechella.domain.entities.usuario.Usuario;
+import br.com.alura.codechella.infra.persistence.UsuarioEntity;
+
+import java.util.List;
+
+public interface RepositorioDeUsuario {
+
+    Usuario cadastrarUsuario(Usuario usuario);
+
+    List<UsuarioEntity> listarTodos();
+}
